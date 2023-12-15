3fa40b80e73361df6caf6806864f938c7e6c117c
kdarlishta <72201300+kdarlishta@users.noreply.github.com>
2023-12-15 23:59:08 +0100

    Changes on api controller

---
+++ app/controllers/api/v1/proions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class ProionsController < ApplicationController
+      def create
+        proions_params.each do |proion_params|
+          proion = Proion.find_or_initialize_by(market_proion_id: proion_params[:market_proion_id])
+          proion.update(proion_params)
+        end
+
+        head :ok
+      end
+
+      private
+
+      def proions_params
+        params.require(:proions).permit(proions: %i[market_proion_id title price])
+          .fetch(:proions, [])
+      end
+    end
+  end
+end
