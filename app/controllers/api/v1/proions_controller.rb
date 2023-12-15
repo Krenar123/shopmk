5a4ac4ae9c8ff158a2b7662f1877f47b1e8f3326
kdarlishta <72201300+kdarlishta@users.noreply.github.com>
2023-12-15 23:27:47 +0100

    Changes on api

--- app/controllers/api/v1/proions_controller.rb
+++ app/controllers/api/v1/proions_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class ProionsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      
       def create
         proions_params.each do |proion_params|
           proion = Proion.find_or_initialize_by(market_proion_id: proion_params[:market_proion_id])
